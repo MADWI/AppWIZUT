3455d123d5bb5824c6af92a5a8e2057f24e5baa5
Rafał Mielnik <rmielnik10@gmail.com>
2013-11-18 18:33:04 +0100

    Create Advertisements.java

---
+++ src/pl/edu/zut/mad/appwizut/models/fragments/Advertisements.java
@@ -0,0 +1,12 @@
+package pl.edu.zut.mad.appwizut.models.fragments;
+
+import pl.edu.zut.mad.appwizut.connection.HTTPLinks;
+
+public class Advertisements extends UpdateAndSetNews {
+
+	public Advertisements() {
+		super();
+		super.setHttpLink(HTTPLinks.OGLOSZENIA);
+		super.startConnect();
+	}
+}
